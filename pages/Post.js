e52adf61c5073c76496cab0b470fe8f0523dff0c
Dun Yan <97597966+dunyanong@users.noreply.github.com>
2022-11-30 13:12:15 +0800

    Update Post.js

--- pages/Post.js
+++ pages/Post.js
@@ -81,18 +81,18 @@ const Post = () => {
 
     return  (
       <div>
-      <nav className="py-10 mb-12 flex justify-between items-center">
-      <ul className="flex">
-      <li>
-        <Link href="/" legacyBehavior>
-          <button>
-            <h1 className="text-4xl md:text-7xl text-cyan-700 font-cormorant italic">Confessay</h1>
-          </button>
-        </Link>
-      </li>
-      </ul>
-      <FullScreenNavbar />
-      </nav>
+        <nav className="py-10 mb-12 flex justify-between items-center ">
+            <ul className="flex">
+            <li>
+            <Link href="/" legacyBehavior>
+                <button>
+                <h1 className="text-2xl md:text-7xl text-cyan-700 font-cormorant font-bold italic tracking-wider">Confessay</h1>
+                </button>
+            </Link>
+            </li>
+            </ul>
+            <FullScreenNavbar />
+        </nav>
     
     <div className="my-10 p-12 border-solid rounded-lg max-w-md mx-auto">
     <div className="text-start my-8">
