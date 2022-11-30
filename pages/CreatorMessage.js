f17cd23c2cf38eab63f92b961c586eceaa00b98d
Dun Yan <97597966+dunyanong@users.noreply.github.com>
2022-11-30 22:27:36 +0800

    Update CreatorMessage.js

--- pages/CreatorMessage.js
+++ pages/CreatorMessage.js
@@ -88,9 +88,9 @@ const CreatorMessage = () => {
                 <h2 className="text-3xl text-start font-medium font-semibold text-cyan-700">Questions</h2>
             </div>
 
-            <div className="font-medium" id="accordion-open" data-accordion="open">
+            <div className="font-medium " id="accordion-open" data-accordion="open">
             <h2 id="accordion-open-heading-2">
-                <button type="button" onClick={changeStyle1} className="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-gray-200 hover:bg-gray-100" data-accordion-target="#accordion-open-body-2" aria-expanded="false" aria-controls="accordion-open-body-2">
+                <button type="button" onClick={changeStyle1} className="rounded-t-lg flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-gray-200 hover:bg-gray-100" data-accordion-target="#accordion-open-body-2" aria-expanded="false" aria-controls="accordion-open-body-2">
                 <span className="flex items-center font-medium"><svg className="w-5 h-5 mr-2 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd"></path></svg>Why is Confessay created?</span>
                 <svg data-accordion-icon className="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                 </button>
@@ -128,13 +128,13 @@ const CreatorMessage = () => {
             </div>
 
             <h2 id="accordion-open-heading-2">
-                <button type="button" onClick={changeStyle4} className="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-gray-200 hover:bg-gray-100" data-accordion-target="#accordion-open-body-2" aria-expanded="false" aria-controls="accordion-open-body-2">
+                <button type="button" onClick={changeStyle4} className="rounded-b-lg flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-gray-200 hover:bg-gray-100" data-accordion-target="#accordion-open-body-2" aria-expanded="false" aria-controls="accordion-open-body-2">
                 <span className="flex items-center font-medium"><svg className="w-5 h-5 mr-2 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd"></path></svg>Am I anonymous in the confession posts?</span>
                 <svg data-accordion-icon className="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                 </button>
             </h2>
             <div id="accordion-open-body-2" className={style4} aria-labelledby="accordion-open-heading-2">
-                <div className="p-5 font-light border border-gray-200">
+                <div className="p-5 font-light border border-gray-200 rounded-b-lg">
                 <p className="mb-2 text-gray-500 font-medium">Your username will not be displayed on the post to protect your identity. However, it will be shown if you comment under any post including your own.</p>
                 </div>
             </div>
