ffd6a65d2c90b877319096d0f0f44ed04437bd9e
Dun Yan <97597966+dunyanong@users.noreply.github.com>
2022-12-01 02:29:50 +0800

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import Footer  from "./Footer";
 const Layout = ({children}) => {
     return (
         <div>
-            <main className="px-10 md:px-20">{children}</main>
+            <main className="px-5 md:px-20">{children}</main>
             <Footer />
         </div>
     );
