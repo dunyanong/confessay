3471370753cec58aa605feac541bc0d01deb08c2
Dun Yan <97597966+dunyanong@users.noreply.github.com>
2022-12-01 00:08:30 +0800

    Update Post.js

--- pages/Post.js
+++ pages/Post.js
@@ -93,7 +93,7 @@ const Post = () => {
             </ul>
             <FullScreenNavbar />
         </nav>
-    <div className="mt-2 my-10 md:my-10 p-12 border-solid rounded-lg max-w-lg mx-auto">
+    <div className="mt-2 my-10 md:my-10 p-12 border-solid rounded-lg max-w-xl mx-auto">
     <div className="text-start mb-8 ">
       <h3 className="text-2xl font-bold font-medium">Rules</h3>
       <div>
