33ba9897141667e7ae9df1221b150918846ef44c
Dun Yan <97597966+dunyanong@users.noreply.github.com>
2022-12-01 02:28:04 +0800

    Update Dashboard.js

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -66,7 +66,7 @@ export default function Dashboard() {
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
-      <nav className="py-10 mb-12 flex justify-between items-center ">
+      <nav className="py-10 mb-12 flex justify-between items-center px-5">
           <ul className="flex">
           <li>
           <Link href="/" legacyBehavior>
